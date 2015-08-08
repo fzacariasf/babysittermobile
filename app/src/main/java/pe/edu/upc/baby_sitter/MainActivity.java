45c4936318455c94cf3e72b0d6d2cf292bc0de92
YasminCondor <yasminpcl@hotmail.com>
2015-08-08 06:16:11 -0500

    Lista de babysitters

---
+++ app/src/main/java/pe/edu/upc/baby_sitter/MainActivity.java
@@ -0,0 +1,55 @@
+package pe.edu.upc.baby_sitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+
+import pe.edu.upc.baby_sitter.R;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    Button peopleButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        peopleButton = (Button) findViewById(R.id.peopleButton);
+        peopleButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.peopleButton) {
+            this.startActivity(new Intent(MainActivity.this, CatalogActivity.class));
+
+        }
+    }
+}
---
+++ app/src/main/java/pe/edu/upc/baby_sitter/Person.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.baby_sitter;
+
+/**
+ * Created by Rosario on 08/08/2015.
+ */
+public class Person {
+
+    String firstName;
+    String lastName;
+    String pictureUrl;
+
+    public Person(String firstName, String lastName, String pictureUrl) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pictureUrl = pictureUrl;
+    }
+}
